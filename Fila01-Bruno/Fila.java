f95be3b512bed00439b5a2b3dfb41f4d897b8ed9
BrunoUnipam <brunoazevedo1@unipam.edu.br>
2025-09-04 18:45:59 +0000

    atividade

---
+++ Fila01-Bruno/Fila.java
@@ -0,0 +1,53 @@
+
+public class Fila<T> {
+    private No<T> primeiroNo;
+    private No<T> ultimoNo;
+    private String nomeFila;
+//---------------------------------------------------------------------------------------------------------------//
+public Fila(){
+    this("Fila");
+}
+ public Fila(String nomeFila){
+        this.nomeFila = nomeFila;
+        this.primeiroNo =null;
+        this.ultimoNo =null; 
+    }
+
+//----------------------------------------------------------------------------------------------------------//
+public void addFinal(T dado){
+    No<T> novoNo = new No<T>(dado);
+    if (primeiroNo==null) {
+        primeiroNo = ultimoNo = novoNo;
+    }
+    else{
+        ultimoNo.setNextNo(novoNo);
+        ultimoNo = novoNo;
+    }
+}
+//----------------------------------------------------------------------------------------------------//
+    public void imprimeLista(){
+
+            if(primeiroNo==null){
+        System.out.println("Lista Vazia");
+        } 
+        
+        else {
+          No<T> aux = primeiroNo;    
+
+        while (aux !=null) {
+            System.out.print(aux.toString()+" ");
+            aux = aux.getNextNo();
+            System.out.println();
+        }
+
+         }
+          }
+
+//----------------------------------------------------------------------------------------------------//
+
+
+
+//----------------------------------------------------------------------------------------------------//
+
+
+}
